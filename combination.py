b98fd3ba31f879ed61c6496875858604210038da
Seeratul <126798754+Seeratul@users.noreply.github.com>
2023-05-09 12:48:31 +0200

    It was just a ssqueeze

--- combination.py
+++ combination.py
@@ -5,8 +5,8 @@ import numpy as np
 def combination(a1,a2,axis = 0):
     # delete the NotImplementedError when you write your function.
     try: 
-         #a1 = np.squeeze(a1)
-         #a2 = np.squeeze(a2)
+         a1 = np.squeeze(a1)
+         a2 = np.squeeze(a2)
          return np.concatenate((a1,a2),axis)
 
     except:
@@ -15,8 +15,8 @@ def combination(a1,a2,axis = 0):
 
 
 if __name__ == "__main__":
-    # use this for your own testing!
-    #a1 = np.ones((2,2))
-    #a2 = np.ones((2,2))
-    #print(combination(a1,a2))
+    #use this for your own testing!
+    a1 = np.ones((2,2))
+    a2 = np.array([[[[1, 2], [3, 4], [5, 6]]]])
+    print(combination(a1,a2))
     pass

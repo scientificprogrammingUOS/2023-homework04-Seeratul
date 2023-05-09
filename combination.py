42aeb83f46ec1a0972214533ce56840a5b98d81f
Seeratul <126798754+Seeratul@users.noreply.github.com>
2023-05-09 12:29:59 +0200

    np.concatonate

--- combination.py
+++ combination.py
@@ -2,9 +2,14 @@ import numpy as np
 
 # implement your function to combine two numpy arrays 
 
-def combine():
+def combine(a1,a2,axis = 0):
     # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
+    try: 
+         return np.concatenate(a1,a2,axis)
+
+    except:
+        print ("The combination appears to be impossible!")
+        return None
 
 
 if __name__ == "__main__":
